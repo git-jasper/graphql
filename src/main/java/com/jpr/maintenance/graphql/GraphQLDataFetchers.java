dff116316aca0cc4c4d31ceca10534fb23ed98f6
jasper <jasperbinda@gmail.com>
2021-09-06 19:46:57 +0200

    refactor createDetails to handle errors

--- src/main/java/com/jpr/maintenance/graphql/GraphQLDataFetchers.java
+++ src/main/java/com/jpr/maintenance/graphql/GraphQLDataFetchers.java
@@ -2,9 +2,13 @@ package com.jpr.maintenance.graphql;
 
 import com.jpr.maintenance.database.model.TaskDetailsEntity;
 import com.jpr.maintenance.database.service.TaskDetailsService;
+import com.jpr.maintenance.validation.errors.InputValidationError;
 import com.jpr.maintenance.validation.model.taskdetails.TaskDetails;
+import graphql.execution.DataFetcherResult;
 import graphql.schema.DataFetcher;
 import org.springframework.stereotype.Component;
+
+import java.util.function.Consumer;
 
 @Component
 public class GraphQLDataFetchers {
@@ -15,6 +19,7 @@ public class GraphQLDataFetchers {
         this.service = service;
     }
 
+    //TODO refactor to return DataFetcherResult
     public DataFetcher<TaskDetailsEntity> getTaskDetailsById() {
         return dataFetchingEnvironment -> {
             String taskId = dataFetchingEnvironment.getArgument("task_id");
@@ -22,15 +27,28 @@ public class GraphQLDataFetchers {
         };
     }
 
-    public DataFetcher<TaskDetailsEntity> createTaskDetails() {
+    public DataFetcher<DataFetcherResult<TaskDetails>> createTaskDetails() {
         return dataFetchingEnvironment -> {
-            var either = TaskDetails.of(dataFetchingEnvironment);
-            TaskDetailsEntity entity = either.map(TaskDetailsEntity::of).get();
-            return service.save(entity);
+            DataFetcherResult.Builder<TaskDetails> resultBuilder = DataFetcherResult.newResult();
+            TaskDetails.of(dataFetchingEnvironment)
+                .peekLeft(handleErrors())
+                .peek(details -> {
+                    service.save(TaskDetailsEntity.of(details));
+                    resultBuilder.data(details);
+                });
+            return resultBuilder.build();
         };
     }
 
-    public DataFetcher<Void> deleteTaskDetails() { //TODO is there a better way than return type Void?
+    // TODO move error handling to its own class
+    private Consumer<InputValidationError> handleErrors() {
+        return error -> {
+            System.out.println(error.name());
+        };
+    }
+
+    //TODO refactor to return DataFetcherResult
+    public DataFetcher<Void> deleteTaskDetails() {
         return dataFetchingEnvironment -> {
             String task_id = dataFetchingEnvironment.getArgument("task_id");
             service.deleteById(Integer.valueOf(task_id));
