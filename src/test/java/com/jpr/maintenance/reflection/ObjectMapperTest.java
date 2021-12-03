4b70a26912c1ae9af8b226ec8b12b0e8dcc77f26
jonckvanderkogel <jonckvanderkogel@gmail.com>
2021-12-03 18:20:34 +0100

    fixed ObjectMapper tests

--- src/test/java/com/jpr/maintenance/reflection/ObjectMapperTest.java
+++ src/test/java/com/jpr/maintenance/reflection/ObjectMapperTest.java
@@ -1,5 +1,6 @@
 package com.jpr.maintenance.reflection;
 
+import com.jpr.maintenance.graphql.model.MotorcycleInput;
 import graphql.GraphQLError;
 import io.vavr.control.Either;
 import lombok.Getter;
@@ -10,97 +11,97 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.jpr.maintenance.validation.errors.InputValidationError.FAILED_TO_INSTANTIATE_OBJECT;
-import static com.jpr.maintenance.validation.errors.InputValidationError.FAILED_TO_RESOLVE_FIELD;
+import static com.jpr.maintenance.model.Brand.DUCATI;
+import static com.jpr.maintenance.validation.errors.InputValidationError.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ObjectMapperTest {
 
-//    @Test
-//    public void toObjectOk(){
-//        Map<String, Object> map = Map.of(
-//            "interval_km", 1,
-//            "description", "test",
-//            "interval_months", 2
-//        );
-//        TaskDetailsInput taskDetailsInput = ObjectMapper.toObject(map, TaskDetailsInput.class).get();
-//
-//        assertEquals("test", taskDetailsInput.getDescription());
-//        assertEquals(1, taskDetailsInput.getInterval_km());
-//        assertEquals(2, taskDetailsInput.getInterval_months());
-//    }
-//
-//    @Test
-//    public void toObjectFailedToResolveField(){
-//        Map<String, Object> map = Map.of("name", "test");
-//        GraphQLError error = ObjectMapper.toObject(map, MissingConstructor.class).getLeft();
-//
-//        assertEquals(FAILED_TO_INSTANTIATE_OBJECT, error.getErrorType());
-//    }
-//
-//    @Test
-//    public void toObjectFailedToInstantiateObject(){
-//        Map<String, Object> map = new HashMap<>();
-//        GraphQLError error = ObjectMapper.toObject(map, TaskDetailsInput.class).getLeft();
-//
-//        assertEquals(FAILED_TO_RESOLVE_FIELD, error.getErrorType());
-//    }
-//
-//    @Test
-//    public void toObjectWithRef(){
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("name", "main");
-//        map.put("ref", Map.of("name", "ref", "text", "text"));
-//        Either<GraphQLError, ClassWithRef> either = ObjectMapper.toObject(map, ClassWithRef.class);
-//
-//        assertTrue(either.isRight());
-//        ClassWithRef classWithRef = either.get();
-//
-//        assertEquals("main", classWithRef.getName());
-//        assertEquals("ref", classWithRef.getRef().getName());
-//    }
-//
-//    @Test
-//    public void toObjectWithList(){
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("name", "main");
-//        map.put("refs", List.of(Map.of("name", "ref1"), Map.of("name", "ref2")));
-//        Either<GraphQLError, ClassWithListOfRef> either = ObjectMapper.toObject(map, ClassWithListOfRef.class);
-//
-//        assertTrue(either.isRight());
-//        ClassWithListOfRef outerClass = either.get();
-//
-//        assertEquals("main", outerClass.getName());
-//        assertEquals("ref1", outerClass.getRefs().get(0).getName());
-//        assertEquals("ref2", outerClass.getRefs().get(1).getName());
-//    }
-//
-//    static class MissingConstructor {
-//        private String name;
-//
-//        public MissingConstructor() {
-//        }
-//    }
-//
-//    @Getter
-//    @RequiredArgsConstructor
-//    static class ClassWithRef {
-//        private final String name;
-//        private final RefClass ref;
-//    }
-//
-//    @Getter
-//    @RequiredArgsConstructor
-//    static class ClassWithListOfRef {
-//        private final String name;
-//        private final List<RefClass> refs;
-//    }
-//
-//    @Getter
-//    @RequiredArgsConstructor
-//    static class RefClass {
-//        private final String name;
-//    }
+    @Test
+    public void toObjectOk(){
+        Map<String, Object> map = Map.of(
+            "brand", "DUCATI",
+            "name", "999R",
+            "engineSize", 999
+        );
+        MotorcycleInput input = ObjectMapper.toObject(map, MotorcycleInput.class).get();
+
+        assertEquals(DUCATI, input.brand());
+        assertEquals("999R", input.name());
+        assertEquals(999, input.engineSize());
+    }
+
+    @Test
+    public void toObjectFailedToResolveField(){
+        Map<String, Object> map = Map.of("name", "test");
+        GraphQLError error = ObjectMapper.toObject(map, MissingConstructor.class).getLeft();
+
+        assertEquals(FAILED_TO_INSTANTIATE_OBJECT, error.getErrorType());
+    }
+
+    @Test
+    public void toObjectFailedToInstantiateObject(){
+        Map<String, Object> map = new HashMap<>();
+        GraphQLError error = ObjectMapper.toObject(map, MotorcycleInput.class).getLeft();
+
+        assertEquals(NULL_VALUE, error.getErrorType());
+    }
+
+    @Test
+    public void toObjectWithRef(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "main");
+        map.put("ref", Map.of("name", "ref", "text", "text"));
+        Either<GraphQLError, ClassWithRef> either = ObjectMapper.toObject(map, ClassWithRef.class);
+
+        assertTrue(either.isRight());
+        ClassWithRef classWithRef = either.get();
+
+        assertEquals("main", classWithRef.getName());
+        assertEquals("ref", classWithRef.getRef().getName());
+    }
+
+    @Test
+    public void toObjectWithList(){
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "main");
+        map.put("refs", List.of(Map.of("name", "ref1"), Map.of("name", "ref2")));
+        Either<GraphQLError, ClassWithListOfRef> either = ObjectMapper.toObject(map, ClassWithListOfRef.class);
+
+        assertTrue(either.isRight());
+        ClassWithListOfRef outerClass = either.get();
+
+        assertEquals("main", outerClass.getName());
+        assertEquals("ref1", outerClass.getRefs().get(0).getName());
+        assertEquals("ref2", outerClass.getRefs().get(1).getName());
+    }
+
+    static class MissingConstructor {
+        private String name;
+
+        public MissingConstructor() {
+        }
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    static class ClassWithRef {
+        private final String name;
+        private final RefClass ref;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    static class ClassWithListOfRef {
+        private final String name;
+        private final List<RefClass> refs;
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    static class RefClass {
+        private final String name;
+    }
 
 }
