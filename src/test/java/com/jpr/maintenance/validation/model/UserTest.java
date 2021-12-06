8853c767ce54781c626e31f649467afee1f503fb
jasper <jasperbinda@gmail.com>
2021-12-06 16:26:28 +0100

    re-add deleted tests

--- src/test/java/com/jpr/maintenance/validation/model/UserTest.java
+++ src/test/java/com/jpr/maintenance/validation/model/UserTest.java
@@ -1,8 +1,11 @@
 package com.jpr.maintenance.validation.model;
 
 import com.jpr.maintenance.graphql.model.UserInput;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static com.jpr.maintenance.validation.errors.InputValidationError.INVALID_FIELD;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UserTest {
 
@@ -11,9 +14,27 @@ public class UserTest {
         var userInput = new UserInput("myName", "myPassword");
         var userEither = User.of(userInput);
 
-        Assertions.assertTrue(userEither.isRight());
+        assertTrue(userEither.isRight());
         var user = userEither.get();
-        Assertions.assertEquals("myName", user.username());
-        Assertions.assertEquals("myPassword", user.password());
+        assertEquals("myName", user.username());
+        assertEquals("myPassword", user.password());
+    }
+
+    @Test
+    void invalidName() {
+        var userInput = new UserInput("", "myPassword");
+        var userEither = User.of(userInput);
+
+        assertTrue(userEither.isLeft());
+        assertEquals(INVALID_FIELD, userEither.getLeft().getErrorType());
+    }
+
+    @Test
+    void invalidPassword() {
+        var userInput = new UserInput("myName", "");
+        var userEither = User.of(userInput);
+
+        assertTrue(userEither.isLeft());
+        assertEquals(INVALID_FIELD, userEither.getLeft().getErrorType());
     }
 }
